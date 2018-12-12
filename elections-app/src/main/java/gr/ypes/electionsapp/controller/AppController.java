6ceb89ff4d0d58dfef0b3df6d58900681af2f4ea
mgourlis <myrgourlis@gmail.com>
2018-12-12 15:59:57 +0200

    3

--- elections-app/src/main/java/gr/ypes/electionsapp/controller/AppController.java
+++ elections-app/src/main/java/gr/ypes/electionsapp/controller/AppController.java
@@ -7,6 +7,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.keycloak.KeycloakPrincipal;
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,8 +25,10 @@ public class AppController {
 	@RequestMapping(value = "/user", method = RequestMethod.GET)
 	public String getUser(Principal principal, Model model) {
 		model.addAttribute("user", principal.getName());
+		String str = principal.getName() + "<br/>";
+		str += ((KeycloakAuthenticationToken) principal).getAuthorities().toString();
 		if(!principal.getName().isEmpty())
-			return principal.getName();
+			return str;
 		return "user";
 	}
 
